78efcaff3e2e17f077fb8e440322a87698bddf52
Hitesh Kumar Sharma <hiteshupes@gmail.com>
2023-01-31 15:51:04 +0530

    test

--- src/main/java/pkg1/MyClass.java
+++ src/main/java/pkg1/MyClass.java
@@ -4,6 +4,10 @@ public class MyClass {
 	{
 		System.out.println("Hello DevOps! Welcome to Jenkins");
 	}
+	public void diplay()
+	{
+		System.out.println("Hello World!");
+	}
 	public static void main(String[] args) {
 
 		MyClass ob = new MyClass();
