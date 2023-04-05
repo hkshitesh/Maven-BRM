c615efff466c653c010f79e06c148c9d276faefe
Hitesh Kumar Sharma <hiteshupes@gmail.com>
2023-04-05 19:07:50 +0530

    Hello World Added

--- src/main/java/pkg1/MyClass.java
+++ src/main/java/pkg1/MyClass.java
@@ -3,6 +3,7 @@ public class MyClass {
 	public void show()
 	{
 		System.out.println("Hello GFG! Welcome to Jenkins");
+		System.out.println("Hello World! Welcome to Jenkins");
 	}
 	public static void main(String[] args) 
 	{
